c2ed3da36a99982629375b061f10798e8148b764
lcimsh <qimsh7@gmail.com>
2022-07-17 16:28:22 +0900

    make main page & about me page

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
     path('blog/', include('blog.urls')),
 ]
