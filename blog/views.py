c977a3f1745e989efb94cf95769f5c2ab4f0f1d8
lcimsh <qimsh7@gmail.com>
2022-07-27 15:03:55 +0900

    add Category to Post detail page

--- blog/views.py
+++ blog/views.py
@@ -10,10 +10,18 @@ class PostList(ListView):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
         return context
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        
+        return context
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
