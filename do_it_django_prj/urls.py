6c4bf2b8c22ce091d6534120d3f30245b2de4bc3
lcimsh <qimsh7@gmail.com>
2022-08-02 17:02:04 +0900

    apply markdown to Content form

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('markdownx/', include('markdownx.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
